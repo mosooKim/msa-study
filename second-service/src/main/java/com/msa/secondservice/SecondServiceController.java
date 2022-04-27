7edd0afe5cbaf145fd0602d75b24825cb7c7a434
mosooKim <ahtn21@gmail.com>
2022-04-27 15:31:49 +0900

    second-service

---
+++ second-service/src/main/java/com/msa/secondservice/SecondServiceController.java
@@ -0,0 +1,14 @@
+package com.msa.secondservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SecondServiceController {
+    @GetMapping("/welcome")
+    pubilc String welcome() {
+        return "Welcome to the Second service.";
+    }
+}
